31e9c292b7c82c2b906f21301e73bd524c41c600
Judy Tuan <315395+judytuna@users.noreply.github.com>
2025-08-22 18:13:39 -0700

    fix gradio embed on huggingface

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ export default {
   async fetch(request) {
     const html = `<!DOCTYPE html>
     <head>
-      <script type="module" src="https://gradio.s3-us-west-2.amazonaws.com/3.50.0/gradio.js"></script>
+      <script type="module" src="https://gradio.s3-us-west-2.amazonaws.com/5.35.0/gradio.js"></script>
     </head>
     <body>
-    <gradio-app src="https://huggingface.co/spaces/IntuitiveHorseware/tru-stride-analyzer"></gradio-app>
+    <gradio-app src="https://intuitivehorseware-tru-stride-analyzer.hf.space"></gradio-app>
     </body>`;
 
     return new Response(html, {
